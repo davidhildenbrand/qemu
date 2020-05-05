c82477b075872ac25299914e6224001e20949c6e
David Hildenbrand <david@redhat.com>
2020-05-05 14:54:15 +0200

    virtio-balloon: Rip out qemu_balloon_inhibit()
    The only remaining special case is postcopy. It cannot handle
    concurrent discards yet, which would result in requesting already sent
    pages from the source. Special-case it in virtio-balloon instead.
    
    Introduce migration_in_incoming_postcopy(), to find out if incoming
    postcopy is active.
    
    Cc: "Michael S. Tsirkin" <mst@redhat.com>
    Cc: Juan Quintela <quintela@redhat.com>
    Cc: "Dr. David Alan Gilbert" <dgilbert@redhat.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>

--- hw/virtio/virtio-balloon.c
+++ hw/virtio/virtio-balloon.c
@@ -63,6 +63,12 @@ static bool virtio_balloon_pbp_matches(PartiallyBalloonedPage *pbp,
     return pbp->base_gpa == base_gpa;
 }
 
+static bool virtio_balloon_inhibited(void)
+{
+    /* Postcopy cannot deal with concurrent discards, so it's special. */
+    return ram_block_discard_is_disabled() || migration_in_incoming_postcopy();
+}
+
 static void balloon_inflate_page(VirtIOBalloon *balloon,
                                  MemoryRegion *mr, hwaddr mr_offset,
                                  PartiallyBalloonedPage *pbp)
@@ -360,7 +366,7 @@ static void virtio_balloon_handle_output(VirtIODevice *vdev, VirtQueue *vq)
 
             trace_virtio_balloon_handle_output(memory_region_name(section.mr),
                                                pa);
-            if (!qemu_balloon_is_inhibited()) {
+            if (!virtio_balloon_inhibited()) {
                 if (vq == s->ivq) {
                     balloon_inflate_page(s, section.mr,
                                          section.offset_within_region, &pbp);
