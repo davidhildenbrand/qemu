45562804754fd118c9e8790f04b35a80a5e0c8c9
David Hildenbrand <david@redhat.com>
2020-06-05 14:05:21 +0200

    numa: Auto-enable NUMA when any memory devices are possible
    Let's auto-enable it also when maxmem is specified but no slots are
    defined. This will result in us properly creating ACPI srat tables,
    indicating the maximum possible PFN to the guest OS. Based on this, e.g.,
    Linux will enable the swiotlb properly.
    
    This avoids having to manually force the switolb on (swiotlb=force) in
    Linux in case we're booting only using DMA memory (e.g., 2GB on x86-64),
    and virtio-mem adds memory later on that really needs the swiotlb to be
    used for DMA.
    
    Let's take care of backwards compatibility if somebody has a setup that
    specifies "maxram" without "slots".
    
    Reported-by: Alex Shi <alex.shi@linux.alibaba.com>
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: Eduardo Habkost <ehabkost@redhat.com>
    Cc: Marcel Apfelbaum <marcel.apfelbaum@gmail.com>
    Cc: Sergio Lopez <slp@redhat.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Richard Henderson <rth@twiddle.net>
    Cc: "Michael S. Tsirkin" <mst@redhat.com>
    Cc: Igor Mammedov <imammedo@redhat.com>
    Cc: qemu-arm@nongnu.org <qemu-arm@nongnu.org>
    Signed-off-by: David Hildenbrand <david@redhat.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2265,6 +2265,7 @@ static void virt_machine_class_init(ObjectClass *oc, void *data)
     mc->numa_mem_supported = true;
     mc->nvdimm_supported = true;
     mc->auto_enable_numa_with_memhp = true;
+    mc->auto_enable_numa_with_memdev = true;
     mc->default_ram_id = "mach-virt.ram";
 
     object_class_property_add(oc, "acpi", "OnOffAuto",
@@ -2375,6 +2376,7 @@ DEFINE_VIRT_MACHINE_AS_LATEST(5, 1)
 static void virt_machine_5_0_options(MachineClass *mc)
 {
     virt_machine_5_1_options(mc);
+    mc->auto_enable_numa_with_memdev = false;
 }
 DEFINE_VIRT_MACHINE(5, 0)
 
